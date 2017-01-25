42e26eff8c5e5a7a0efa188bd2c4aab0716f966a
heaun.b <developer.heb@gmail.com>
2017-01-25 15:10:09 +0900

    hello world commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello:) Main() ");
+            string strMsg = "string messag";
+            HelloWorld.Program.MsgWrite(strMsg);
+        }
+
+        public static void MsgWrite(string msg) {
+            Console.WriteLine("param : " + msg); 
+        }
+    } 
+}
