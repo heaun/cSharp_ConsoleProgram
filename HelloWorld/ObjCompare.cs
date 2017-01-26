8972b36aa8bfdd25b74d081a37ee55042025504a
heaun.b <developer.heb@gmail.com>
2017-01-26 13:50:14 +0900

    Object Compare 추가

---
+++ HelloWorld/ObjCompare.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld
+{
+    class ObjCompare
+    {
+        public static void Main(string[] args) {
+            string strBuffer, strCompare;
+            bool strTureFalse = false;
+
+            strBuffer = "삼각형";
+            strCompare = string.Copy(strBuffer);
+
+            Console.WriteLine("strBuffer / strCompare : {0} / {1}", strBuffer, strCompare);
+
+            strTureFalse = strBuffer.Equals(strCompare);
+            Console.WriteLine("if strBuffer = strCompare ? {0}", strBuffer.Equals(strCompare));
+
+            Console.WriteLine("isEqualObject ? {0}", (object)strBuffer == (object)strCompare);
+            Console.WriteLine("Object strBuffer Type ? {0}", strBuffer.GetType());
+            Console.WriteLine("compare strBuffer with strBuffer ? {0}", strBuffer.CompareTo(strBuffer));
+
+        }
+    }
+}
