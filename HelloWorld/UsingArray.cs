3324eaf241d6f8b5757abb28a0da27aa1b473902
heaun.b <developer.heb@gmail.com>
2017-01-26 14:05:34 +0900

    Using Array 추가

---
+++ HelloWorld/UsingArray.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld
+{
+    class UsingArray
+    {
+        public static void Main(string[] args) {
+
+            string[] strArray = new string[3]{"monday", "tuesday", "wednesday"};
+            string strVal = "스트링";
+            int intVal = 100;
+
+            /*
+             * 변수/구조체 : call by value
+             * 베열        : call by reference 
+             */
+            Console.WriteLine("main string 변수 : {0}", strVal);
+            Console.WriteLine("main int 변수 : {0}", intVal);
+
+            // 일반변수를 참조용으로 넘길때 ref 사용한다. 
+            ArrayPrint(strArray, ref strVal, intVal);
+
+        }
+
+        private static void ArrayPrint(string[] strArray, ref string paramString, int paramInt)
+        {
+            int i = 0;
+            Console.WriteLine("main에서 온 string 변수 : {0}", paramString);
+            foreach(string a in strArray){
+                Console.WriteLine("strArray[{0}] = {1}", i, a);
+                i++;
+            }
+
+            paramString = "arrayPrint 값 변경";
+            paramInt = 200;
+
+            Console.WriteLine("ArrayPrint string 변수 : {0}", paramString);
+            Console.WriteLine("ArrayPrint int 변수 : {0}", paramInt); 
+        }
+    }
+}
